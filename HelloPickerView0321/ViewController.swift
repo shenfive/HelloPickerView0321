c0878813eea394d25b7c5e5a4b52a9365c30f84d
Danny Shen <shenfive@gmail.com>
2020-03-21 10:17:24 +0800

    Data Picker

--- HelloPickerView0321/ViewController.swift
+++ HelloPickerView0321/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource {
 
+    
+    
+    
+    @IBOutlet weak var thePickerview: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        thePickerview.dataSource = self
     }
 
+    
+    //MARK: Picker View Data Source
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        var number = 1
+        switch component {
+        case 0:
+            number = 5
+        case 1:
+            number = 3
+        default:
+            break
+        }
+        return number
+    }
 
 }
 
